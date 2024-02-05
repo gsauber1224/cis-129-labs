4bb7554aa6cf214e868454e6f1df26ce41f734a7
gsauber1224 <158348537+gsauber1224@users.noreply.github.com>
2024-02-05 16:42:59 -0700

    Bio added in cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,4 @@
-print('Hello, World!')
+# Author: Grace Sauber
+# My first Python lab in my CIS129 course
+# Prints a simple little bio 
+print('My name is Grace. I am 22 years old. I like being 22 because I like even numbers.')
